bf42a743b1e68db089b12181abfd9f9512aa6ced
Belma-Karatas <belmakrts_00@hotmail.com>
2025-06-09 20:37:45 +0300

    İlk backend yüklemesi

---
+++ src/main/java/com/hastane/hastanebackend/HastanebackendApplication.java
@@ -0,0 +1,13 @@
+package com.hastane.hastanebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HastanebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HastanebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hastane/hastanebackend/HastanebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastane.hastanebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HastanebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
